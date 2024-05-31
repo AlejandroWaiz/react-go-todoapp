d859536ac48deb29bc0e94870d77a0e0b67073b6
AlejandroWaiz <alejosaurio1@gmail.com>
2024-05-31 00:08:17 -0400

    Starting to modularizing the router

---
+++ server/router/TodoRequests.go
@@ -0,0 +1,96 @@
+package router
+
+import (
+	"github.com/AlejandroWaiz/go-react-todoapp/model"
+	"github.com/gofiber/fiber/v2"
+)
+
+func (r *Router) postTodo(c *fiber.Ctx) error {
+
+	todo := model.Todo{}
+
+	if err := c.BodyParser(&todo); err != nil {
+		return fiber.NewError(400, "[Router] Invalid Todo body")
+	}
+
+	AllTodo, err := r.db.PostTodo(todo)
+
+	if err != nil {
+		return fiber.NewError(400, err.Error())
+	}
+
+	return c.JSON(AllTodo)
+
+}
+
+func (r *Router) getTodos(c *fiber.Ctx) error {
+
+	AllTodo, err := r.db.GetTodos()
+
+	if err != nil {
+		return fiber.NewError(400, err.Error())
+	}
+
+	return c.JSON(AllTodo)
+
+}
+
+func (r *Router) updateTodo(c *fiber.Ctx) error {
+
+	todo := model.Todo{}
+
+	if err := c.BodyParser(&todo); err != nil {
+		return fiber.NewError(400, "[Router] Invalid Todo body")
+	}
+
+	AllTodo, err := r.db.UpdateTodo(todo)
+
+	if err != nil {
+		return fiber.NewError(400, err.Error())
+	}
+
+	return c.JSON(AllTodo)
+
+}
+
+func (r *Router) deleteTodo(c *fiber.Ctx) error {
+
+	todo := model.Todo{}
+
+	if err := c.BodyParser(&todo); err != nil {
+		return fiber.NewError(400, "[Router] Invalid Todo body")
+	}
+
+	AllTodo, err := r.db.DeleteTodo(todo)
+
+	if err != nil {
+		return fiber.NewError(400, err.Error())
+	}
+
+	return c.JSON(AllTodo)
+
+}
+
+func (r *Router) setTodoAsDone(c *fiber.Ctx) error {
+
+	todo := model.Todo{}
+
+	if err := c.BodyParser(&todo); err != nil {
+		return fiber.NewError(400, "[Router] Invalid Todo body")
+	}
+
+	AllTodo, err := r.db.SetTodoAsDone(todo)
+
+	if err != nil {
+		return fiber.NewError(400, err.Error())
+	}
+
+	return c.JSON(AllTodo)
+
+}
+
+func (r *Router) healtcheck(c *fiber.Ctx) error {
+
+	return c.SendString("Healthcheck ok!")
+
+}
--- server/router/router.go
+++ server/router/router.go
@@ -1,1 +1,43 @@
 package router
+
+import (
+	"log"
+
+	"github.com/AlejandroWaiz/go-react-todoapp/database"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+type Router struct {
+	db database.DatabaseImplementation
+}
+
+type RouterImplementation interface {
+	ListenAndServe()
+}
+
+func New(db database.DatabaseImplementation) RouterImplementation {
+
+	return &Router{db: db}
+
+}
+
+func (r *Router) ListenAndServe() {
+
+	app := fiber.New()
+
+	app.Use(cors.New())
+
+	app.Get("/Healtcheck", r.healtcheck)
+
+	app.Post("/api/todos", r.postTodo)
+
+	app.Get("/api/todos", r.getTodos)
+
+	app.Patch("/api/todos/:id/done", r.setTodoAsDone)
+
+	app.Delete("/api/todos/:id/done", r.deleteTodo)
+
+	log.Fatal(app.Listen(":4000"))
+
+}
